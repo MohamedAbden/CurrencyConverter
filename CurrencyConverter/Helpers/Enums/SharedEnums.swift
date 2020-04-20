dce78f26971456f19e660273e9b469b298187fcd
Mohamed Abd el-latef <mohamedabdel-latef@Abden-Macbook.local>
2020-04-20 07:02:08 +0200

    Add SharedEnums

---
+++ CurrencyConverter/Helpers/Enums/SharedEnums.swift
@@ -0,0 +1,16 @@
+//
+//  SharedEnums.swift
+//  CurrencyConverter
+//
+//  Created by Mohamed Abd el-latef on 4/20/20.
+//  Copyright © 2020 Mohamed Abd el-latef. All rights reserved.
+//
+
+import Foundation
+
+
+enum LoadingType: Int {
+    case Default = 1
+    case Placeholder = 2
+    case PullToRefresh = 3
+}
