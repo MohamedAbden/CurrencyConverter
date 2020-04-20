04fe71af8b6b9974ddaa5e570bec45546634af94
Mohamed Abd el-latef <mohamedabdel-latef@Abden-Macbook.local>
2020-04-20 21:37:19 +0200

    add symbols view controller

---
+++ CurrencyConverter/Modules/SymbolsPicker/SymbolsPickerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsPickerViewController.swift
+//  CurrencyConverter
+//
+//  Created by Mohamed Abd el-latef on 4/20/20.
+//  Copyright © 2020 Mohamed Abd el-latef. All rights reserved.
+//
+
+import UIKit
+
+class SymbolsPickerViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
