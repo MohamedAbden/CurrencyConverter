9a412490fb506c9f00a18ed889d75416000d79df
Mohamed Abd el-latef <mohamedabdel-latef@Abden-Macbook.local>
2020-04-20 05:34:51 +0200

    Add BaseNetworkManager

---
+++ CurrencyConverter/ Model Layer/Managers/BaseNetworkManager.swift
@@ -0,0 +1,15 @@
+
+
+//
+//  BaseNetworkManager.swift
+//  CurrencyConverter
+//
+//  Created by Mohamed Abd el-latef on 4/20/20.
+//  Copyright © 2020 Mohamed Abd el-latef. All rights reserved.
+//
+
+import UIKit
+
+class BaseNetworkManager: NSObject {
+
+}
