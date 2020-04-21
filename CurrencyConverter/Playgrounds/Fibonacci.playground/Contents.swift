1b778b36401592619fa55c56269d78929f618a47
Mohamed Abd el-latef <mohamedabdel-latef@Abden-Macbook.local>
2020-04-21 14:53:04 +0200

    Add Fibonacci Playground

---
+++ CurrencyConverter/Playgrounds/Fibonacci.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+//First Approach: recurisve Approach
+func fibonacciRecurisve(_ number: Int) -> Int {
+    guard number > 1 else { return number }
+    return fibonacciRecurisve(number - 1) + fibonacciRecurisve(number - 2)
+}
+
+let recurisveValue = fibonacciRecurisve(10)
+
+
+//Second Approach: Iterative Approach
+func fibonacciIterative(_ number: Int) -> Int {
+    guard number > 1 else { return number }
+
+    var first = 1
+    var second = 1
+    for index in (2...number) {
+//        var temp = first
+//        first = first + second
+//        second = temp
+        (first,second) = (first+second,first)
+    }
+    return first
+}
+let iterativeValue = fibonacciIterative2(10)
+
+
+
+
+
+
